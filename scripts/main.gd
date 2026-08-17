b691c05f483d7e36e8e32156ace3ff0bf4d27899
Fletchi <juliusblohm@icloud.com>
2026-08-17 17:13:14 +0200

    Add join part of the stuff and add handshake should theoreticly work but it doesnt
    :Lgcri:

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,6 @@ extends Node3D
 @onready var temp_mp_menu: PanelContainer = $"Mutliplayer temp menu/tempMPMenu"
 @onready var adress_entry: LineEdit = $"Mutliplayer temp menu/tempMPMenu/VBoxContainer/AdressEntry"
 
-#oh frick jetzt kommt die erste network scheisse
 const tempPlayerScene = preload("res://scenes/player.tscn")
 const PORT = 9999 #lol
 var enet_peer = ENetMultiplayerPeer.new() #erstellt ein multiplayer peer element und gibt einen zeiger drauf bevor es ohne die var in den abyss verschwindet
@@ -42,13 +41,50 @@ func _on_host_pressed() -> void:
 	
 	enet_peer.create_server(Noray.local_port) #noray will sich lieber selber einen port aussuchen wir müssen das da reinpassen, weil er brauch die information einfach er kann sie sich nicht selber holen also stecken wir sie ihm ins maul zwischen den klammern
 	multiplayer.multiplayer_peer = enet_peer #"Godot, benutze dieses Telefon für alles was Multiplayer ist" (oben ja festgelegt
-
+	
+	Noray.on_connect_nat.connect(nat_connect) # "Noray, wenn jemand per direkter Verbindung kommt, ruf _jemand_kommt_direkt auf"
+	Noray.on_connect_relay.connect(relay_connect) # "Noray, wenn jemand per Relay kommt, ruf _jemand_kommt_per_relay auf"
+	
+	add_player(multiplayer.get_unique_id())
+	multiplayer.peer_connected.connect(add_player) #ich sags nochmal multiplayer.peer_connected ist nur ein signal (hier halt in code) und wenn das abefeuert connecten wir mit .connect halt 'add_player'
+	multiplayer.peer_disconnected.connect(remove_player)
+	
+	
+	
 
 func _on_join_pressed() -> void:
 	temp_mp_menu.hide()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	enet_peer.create_client("localhost", PORT) #das ist erstmal die ip whohin wir uns verbinden sollen, wir sind hier local also ist das fine
+	
+	await Noray.register_remote() #ich sags nochmal await macht das wir so lange bei der funktion bleiben bis wir eine bestätigung haben das sie durch ist
+	
+	Noray.on_connect_nat.connect(join) #probiert zuerst nat weil wenn geht besser weil wir keinen umweg haben wenn nicht dann isses so und dann müsssen wir relay hallo sagen
+	Noray.on_connect_relay.connect(join)
+	
+	var host_oid = adress_entry.text
+	await Noray.connect_nat(host_oid)
+	
+	
+	
+	#enet_peer.create_client("localhost", PORT) #das ist erstmal die ip whohin wir uns verbinden sollen, wir sind hier local also ist das fine
+	#multiplayer.multiplayer_peer = enet_peer
+
+func join(address: String, port: int) -> void: #ich nehme an das wir hier weil da einfach code anfällig ist für nochmal genau spezifizieren was das überhaupt für ein filetpy eist mit dem adress und port mit string und int
+	enet_peer.create_client(address, port, 0, 0, 0, Noray.local_port)
 	multiplayer.multiplayer_peer = enet_peer
+
+
+
+func nat_connect(address: String, port: int) -> void:
+	await PacketHandshake.over_enet_peer(enet_peer, address, port)
+	#Handshake heist das wir einfach sicher stellen das hier bei NAT punchtrhough wirklich sicherstellen das beide router offen sind indem wir uns beide diese sachen austauschen,  und das in der klammer ist die adresse wohin wir das schicken sollen. Handshae ist automatisch da muss man nichts machen.
+	print("Jemand kommt per NAT (direkt): ", address, ":", port)
+	
+
+func relay_connect(address: String, port: int) -> void:
+	await PacketHandshake.over_enet_peer(enet_peer, address, port)
+	# Gleich wie direkt, nur läuft's durch Noray's Server (automatisch)
+	print("Jemand kommt per Relay (durch Noray): ", address, ":", port)
 
 func add_player(peer_id): #soll ne peer id mitnehmen, peer id brauch man zum einen für authority purposes
 	var player = tempPlayerScene.instantiate()
@@ -59,4 +95,4 @@ func add_player(peer_id): #soll ne peer id mitnehmen, peer id brauch man zum ein
 func remove_player(peer_id):
 	var player = get_node_or_null(str(peer_id))
 	if player:
-		queue_free()
+		player.queue_free() #NICHT VERGESSEN
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 
-@onready var _2_nd_screen_multiplayer: Node2D = $"2nd Screen Multiplayer"
+@onready var _2_nd_screen_multiplayer: Node2D = $"2nd Screen Multiplayer (NOT USED IN testMAIN)"
 @onready var _1_st_container: VBoxContainer = $"1st Container"
 
 
 func _on_singleplayer_button_pressed() -> void:
 	print("temp singleplayer button")
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_multiplayer_button_pressed() -> void:
 	print("multiplayer button pressed")
