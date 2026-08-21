11d523a0cff13fc5db7833bfa939f287c40daba5
Fletchi <juliusblohm@icloud.com>
2026-08-21 14:19:29 +0200

    Make it that only if youre a hosts you spin the boxes by urself

--- scripts/spinning_block.gd
+++ scripts/spinning_block.gd
@@ -4,4 +4,5 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.play("spin")
+	if multiplayer.is_server():
+		animation_player.play("spin")
